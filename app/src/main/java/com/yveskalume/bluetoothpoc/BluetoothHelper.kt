1ae4ed5adc82947a57a319e2afa302f563b43b81
yveskalume <yvkalume@gmail.com>
2023-11-02 13:13:32 +0200

    connect to a server with known uid

--- app/src/main/java/com/yveskalume/bluetoothpoc/BluetoothHelper.kt
+++ app/src/main/java/com/yveskalume/bluetoothpoc/BluetoothHelper.kt
@@ -1,55 +1,219 @@
 package com.yveskalume.bluetoothpoc
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothHeadset
-import android.bluetooth.BluetoothProfile
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
-object BluetoothHelper {
+@Composable
+fun rememberBluetoothHelper(): BluetoothHelper {
+    val context = LocalContext.current
+    val helper = remember { BluetoothHelper(context) }
 
-    private var bluetoothHeadset: BluetoothHeadset? = null
-
-    private lateinit var profileListener: BluetoothProfile.ServiceListener
-
-    private lateinit var bluetoothAdapter: BluetoothAdapter
-
-    private val _connectedDevicesFlow: MutableStateFlow<List<BluetoothDevice>> = MutableStateFlow(
-        emptyList()
+    val permissions = mutableListOf(
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
     )
-    val connectedDevicesFlow: StateFlow<List<BluetoothDevice>> = _connectedDevicesFlow.asStateFlow()
 
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        permissions.addAll(
+            listOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN
+            )
+        )
+    }
 
-    @SuppressLint("MissingPermission")
-    fun initialize(context: Context): Boolean {
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
-        if (!bluetoothAdapter.isEnabled) {
-            return false
+    val startActivityLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            helper.startDiscovery()
+        } else {
+            Toast.makeText(
+                context,
+                "Bluetooth is not enabled",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
 
-        profileListener = object : BluetoothProfile.ServiceListener {
 
-            override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
-                if (profile == BluetoothProfile.HEADSET) {
-                    bluetoothHeadset = proxy as BluetoothHeadset
-                    _connectedDevicesFlow.value = bluetoothHeadset!!.connectedDevices
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { results ->
+        if (results.containsValue(false)) {
+            Toast.makeText(
+                context,
+                "Some permissions are missing",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            if (!helper.isBluetoothEnabled) {
+                startActivityLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+            }
+        }
+    }
+
+
+
+    LaunchedEffect(Unit) {
+        permissionLauncher.launch(permissions.toTypedArray())
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            helper.release()
+        }
+    }
+    return helper
+}
+
+@SuppressLint("MissingPermission")
+class BluetoothHelper(private val context: Context) {
+
+    private val bluetoothManager by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager.adapter
+    }
+
+    val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter.isEnabled
+
+
+    private val _isDiscovering = MutableStateFlow(bluetoothAdapter.isDiscovering)
+    val isDiscovering: StateFlow<Boolean> = _isDiscovering.asStateFlow()
+
+    private val _pairedDevicesFlow = MutableStateFlow<Set<BluetoothDevice>>(emptySet())
+    val pairedDevicesFlow: StateFlow<Set<BluetoothDevice>> = _pairedDevicesFlow.asStateFlow()
+
+    private val _scannedDevicesFlow = MutableStateFlow<Set<BluetoothDevice>>(emptySet())
+    val scannedDevicesFlow: StateFlow<Set<BluetoothDevice>> = _scannedDevicesFlow.asStateFlow()
+
+
+    private val _devicePairingWith = MutableStateFlow<String?>(null)
+    val devicePairingWith: StateFlow<String?> = _devicePairingWith.asStateFlow()
+
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    if (hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                        val device: BluetoothDevice? =
+                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                        if (device != null) {
+                            _scannedDevicesFlow.update { devices ->
+                                if (device in devices) devices else devices + device
+                            }
+                        }
+                    }
                 }
             }
+        }
+    }
 
-            override fun onServiceDisconnected(profile: Int) {
-                if (profile == BluetoothProfile.HEADSET) {
-                    bluetoothHeadset = null
-                    _connectedDevicesFlow.value = emptyList()
-                }
+    init {
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        context.registerReceiver(receiver, filter)
+
+        startDiscovery()
+    }
+
+    private fun getPairedDevices() {
+        if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+            return
+        }
+        val pairedDevices = bluetoothAdapter.bondedDevices
+        _pairedDevicesFlow.update {
+            pairedDevices
+        }
+    }
+
+    fun unPairDevice(device: BluetoothDevice) {
+
+    }
+
+    suspend fun pairDevice(device: BluetoothDevice,onFailure: () -> Unit) {
+        return withContext(Dispatchers.IO) {
+            try {
+                _devicePairingWith.update { device.address }
+                val bluetoothSocket = device
+                    .createRfcommSocketToServiceRecord(UUID.fromString("27b7d1da-08c7-4505-a6d1-2459987e5e2d"))
+                cancelDiscovery()
+                bluetoothSocket.connect()
+
+            } catch (e: Exception) {
+                Log.e("BluetoothHelper", e.toString())
+                onFailure()
+            } finally {
+                _devicePairingWith.update { null }
             }
         }
-        bluetoothAdapter.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET)
-        bluetoothAdapter.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothHeadset)
-        return true
+    }
+
+    fun startDiscovery() {
+        _isDiscovering.update { true }
+
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+
+        context.registerReceiver(
+            receiver,
+            IntentFilter(BluetoothDevice.ACTION_FOUND)
+        )
+
+
+        getPairedDevices()
+
+        bluetoothAdapter.startDiscovery()
+        _isDiscovering.update { bluetoothAdapter.isDiscovering }
+    }
+
+
+    private fun cancelDiscovery() {
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+
+        bluetoothAdapter.cancelDiscovery()
+        _isDiscovering.update { bluetoothAdapter.isDiscovering }
+    }
+
+    fun release() {
+        context.unregisterReceiver(receiver)
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
     }
 }
--- app/src/main/java/com/yveskalume/bluetoothpoc/MainActivity.kt
+++ app/src/main/java/com/yveskalume/bluetoothpoc/MainActivity.kt
@@ -1,40 +1,47 @@
 package com.yveskalume.bluetoothpoc
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
 import com.yveskalume.bluetoothpoc.ui.theme.BluetoothPOCTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
 
+    @OptIn(ExperimentalFoundationApi::class)
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkPermissions()
 
         setContent {
             BluetoothPOCTheme {
@@ -42,73 +49,123 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val connectedDevices by BluetoothHelper.connectedDevicesFlow.collectAsState()
+                    val bluetoothHelper = rememberBluetoothHelper()
+                    val pairedDevices by bluetoothHelper.pairedDevicesFlow.collectAsState()
+                    val scannedDevices by bluetoothHelper.scannedDevicesFlow.collectAsState()
+                    val devicePairingWith by bluetoothHelper.devicePairingWith.collectAsState()
 
-                    LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp)) {
-                        items(connectedDevices) {device ->
-                            Column(modifier = Modifier.wrapContentSize()) {
-                                Text(text = "Device Name: ${device.name}")
-                                Text(text = "Device Address: ${device.address}")
-                                Text(text = "Device Type: ${device.type}")
+                    val isDiscovering by bluetoothHelper.isDiscovering.collectAsState()
+
+                    val coroutineScope = rememberCoroutineScope()
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                    ) {
+                        stickyHeader {
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Paired Devices",
+                                style = MaterialTheme.typography.titleLarge,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                        }
+                        items(pairedDevices.toList()) { device ->
+                            Card(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .wrapContentHeight()
+                                        .fillMaxWidth()
+                                        .padding(8.dp)
+                                ) {
+                                    Text(text = "Device Name: ${device.name}")
+                                    Text(text = "Device Address: ${device.address}")
+                                    Button(onClick = {
+                                        bluetoothHelper.unPairDevice(device)
+                                    }) {
+                                        Text(text = "Unpair Device")
+                                    }
+                                }
+                            }
+                        }
+
+
+                        stickyHeader {
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Scanned Devices",
+                                style = MaterialTheme.typography.titleLarge,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                        }
+                        items(scannedDevices.toList()) { device ->
+                            val isPairing = devicePairingWith == device.address
+                            Card(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .wrapContentHeight()
+                                        .fillMaxWidth()
+                                        .padding(8.dp)
+                                ) {
+                                    Text(text = "Device Name: ${device.name}")
+                                    Text(text = "Device Address: ${device.address}")
+                                    Button(
+                                        enabled = devicePairingWith == null,
+                                        onClick = {
+                                            coroutineScope.launch {
+                                                bluetoothHelper.pairDevice(device) {
+                                                    launch(Dispatchers.Main) {
+                                                        Toast.makeText(
+                                                            applicationContext,
+                                                            "Can't connect",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    ) {
+                                        Text(
+                                            text = if (isPairing) {
+                                                "Pairing"
+                                            } else {
+                                                "Pair Device"
+                                            }
+                                        )
+                                        if (isPairing) {
+                                            Spacer(modifier = Modifier.width(2.dp))
+                                            CircularProgressIndicator(modifier = Modifier.size(24.dp))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        item {
+                            AnimatedVisibility(visible = isDiscovering) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    CircularProgressIndicator()
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
-
-    private fun accessBluetooth() {
-        val isBluetoothEnabled = BluetoothHelper.initialize(this)
-
-        if (!isBluetoothEnabled) {
-            enableBluetooth()
-        }
-    }
-
-    private fun enableBluetooth() {
-        val activityResultLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) {
-            it.resultCode
-        }
-
-        activityResultLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
-    }
-
-    private fun checkPermissions() {
-
-        val permissions = mutableListOf(
-            Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-
-            )
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permissions.add(Manifest.permission.BLUETOOTH_CONNECT)
-        }
-
-        val launcher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { results ->
-            if (results.containsValue(false)) {
-                Toast.makeText(
-                    this,
-                    "Some permissions are missing",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                accessBluetooth()
-            }
-        }
-
-        launcher.launch(permissions.toTypedArray())
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
 
